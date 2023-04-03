3215479fde3231691be43a08606f1c1c3527cc82
sam <sam.ojling@northvolt.com>
2023-04-03 11:57:51 +0200

    Fixing redirect issue on edit address

--- app/routes/($lang)/cart.tsx
+++ app/routes/($lang)/cart.tsx
@@ -173,19 +173,19 @@ export async function action({request, context}: ActionArgs) {
   );
 }
 
-export default function CartRoute() {
-  const [root] = useMatches();
-  // @todo: finish on a separate PR
-  return (
-    <div className="grid w-full gap-8 p-6 py-8 md:p-8 lg:p-12 justify-items-start">
-      <Suspense fallback={<CartLoading />}>
-        <Await resolve={root.data?.cart}>
-          {(cart) => <Cart layout="page" cart={cart} />}
-        </Await>
-      </Suspense>
-    </div>
-  );
-}
+// export default function CartRoute() {
+//   const [root] = useMatches();
+//   // @todo: finish on a separate PR
+//   return (
+//     <div className="grid w-full gap-8 p-6 py-8 md:p-8 lg:p-12 justify-items-start">
+//       <Suspense fallback={<CartLoading />}>
+//         <Await resolve={root.data?.cart}>
+//           {(cart) => <Cart layout="page" cart={cart} />}
+//         </Await>
+//       </Suspense>
+//     </div>
+//   );
+// }
 
 /*
   Cart Queries
