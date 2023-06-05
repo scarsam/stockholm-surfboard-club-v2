6e40b19619fa59d03d9facd6fdf0b485f871989b
sam <sam.ojling@northvolt.com>
2023-06-05 14:48:37 +0200

    removed klayvio sdk

--- app/routes/($lang).newsletter.tsx
+++ app/routes/($lang).newsletter.tsx
@@ -1,6 +1,4 @@
 import {type ActionFunction, json} from '@shopify/remix-oxygen';
-// @ts-ignore
-import {Profiles} from 'klaviyo-api';
 
 type ActionData = {
   formError?: string;
@@ -10,63 +8,45 @@ const badRequest = (data: ActionData) => json(data, {status: 400});
 
 export const action: ActionFunction = async ({request, context}) => {
   const formData = await request.formData();
+
   const email = formData.get('email');
 
   try {
-    // @ts-ignore
-    Profiles.subscribeProfiles(
-      JSON.stringify({
-        data: {
-          type: 'profile-subscription-bulk-create-job',
-          attributes: {
-            list_id: 'VhRvqf',
-            custom_source: 'Marketing Event',
-            subscriptions: [
-              {
-                channels: {
-                  email: ['MARKETING'],
-                },
-                email,
-              },
-            ],
-          },
+    const response = await fetch(
+      'https://a.klaviyo.com/api/profile-subscription-bulk-create-jobs/',
+      {
+        method: 'POST',
+        headers: {
+          Authorization: context.env?.PRIVATE_KLAVIYO_NEWSLETTER,
+          accept: 'application/json',
+          revision: '2023-02-22',
+          'content-type': 'application/json',
         },
-      }),
+        body: JSON.stringify({
+          data: {
+            type: 'profile-subscription-bulk-create-job',
+            attributes: {
+              list_id: 'VhRvqf',
+              custom_source: 'Marketing Event',
+              subscriptions: [
+                {
+                  channels: {
+                    email: ['MARKETING'],
+                  },
+                  email,
+                },
+              ],
+            },
+          },
+        }),
+      },
     );
-    // const response = await fetch(
-    //   'https://a.klaviyo.com/api/profile-subscription-bulk-create-jobs/',
-    //   {
-    //     method: 'POST',
-    //     headers: {
-    //       Authorization: context.env?.PRIVATE_KLAVIYO_NEWSLETTER,
-    //       accept: 'application/json',
-    //       revision: '2023-02-22',
-    //       'content-type': 'application/json',
-    //     },
-    //     body: JSON.stringify({
-    //       data: {
-    //         type: 'profile-subscription-bulk-create-job',
-    //         attributes: {
-    //           list_id: 'VhRvqf',
-    //           custom_source: 'Marketing Event',
-    //           subscriptions: [
-    //             {
-    //               channels: {
-    //                 email: ['MARKETING'],
-    //               },
-    //               email,
-    //             },
-    //           ],
-    //         },
-    //       },
-    //     }),
-    //   },
-    // );
 
-    // console.log('d', JSON.stringify(subscribe));
+    console.log('d', JSON.stringify(response));
 
     return json({error: null, ok: true});
   } catch (error: any) {
+    console.log('error', error);
     return badRequest({
       formError: 'Something went wrong. Please try again later.',
     });
