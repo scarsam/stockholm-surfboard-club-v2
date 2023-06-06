2fca17fdc6772e90b4fe86213abf5357d8da17aa
sam <sam.ojling@northvolt.com>
2023-06-06 22:35:24 +0200

    Changing to vercel custo-made api endpoint

--- app/routes/($lang).newsletter.tsx
+++ app/routes/($lang).newsletter.tsx
@@ -12,39 +12,24 @@ export const action: ActionFunction = async ({request, context}) => {
   const formEmail = formData.get('email');
 
   try {
-    const response = await fetch(
-      'https://a.klaviyo.com/api/profile-subscription-bulk-create-jobs/',
-      {
-        method: 'POST',
-        headers: {
-          Authorization: context.env?.PRIVATE_KLAVIYO_NEWSLETTER,
-          accept: 'application/json',
-          revision: '2023-02-22',
-          'content-type': 'application/json',
-        },
-        body: JSON.stringify({
-          data: {
-            type: 'profile-subscription-bulk-create-job',
-            attributes: {
-              list_id: 'VhRvqf',
-              custom_source: 'Marketing Event',
-              subscriptions: [
-                {
-                  channels: {
-                    email: ['MARKETING'],
-                  },
-                  email: formEmail,
-                },
-              ],
-            },
-          },
-        }),
+    const res = await fetch('https://klaviyo-api.vercel.app/api/subscribe', {
+      method: 'POST',
+      headers: {
+        // Authorization: context.env?.PRIVATE_KLAVIYO_NEWSLETTER,
+        accept: 'application/json',
+        // revision: '2023-02-22',
+        'content-type': 'application/json',
       },
-    );
-    console.log({response});
-    console.log({context});
+      body: JSON.stringify({
+        data: {
+          email: formEmail,
+        },
+      }),
+    });
 
-    return json({error: null, ok: true});
+    const data = await res.json();
+
+    return json({error: null, ok: data?.status});
   } catch (error: any) {
     return badRequest({
       formError: 'Something went wrong. Please try again later.',
