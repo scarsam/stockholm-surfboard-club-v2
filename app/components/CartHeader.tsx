d03070a7842678f6e351db05c5dd8fd781a29027
sam <sam.ojling@northvolt.com>
2023-06-10 12:13:19 +0200

    removed focus ring in cart

--- app/components/CartHeader.tsx
+++ app/components/CartHeader.tsx
@@ -8,7 +8,7 @@ export const DrawerHeader = ({
   return (
     <div className="flex justify-between p-[calc(0.5em-1px)] md:p-[calc(0.5em-0.5px)] border-b border-t w-full">
       <h2 className="font-semibold">{title}</h2>
-      <button onClick={onClose} type="button">
+      <button className="focus:ring-0" onClick={onClose} type="button">
         Close
       </button>
     </div>
