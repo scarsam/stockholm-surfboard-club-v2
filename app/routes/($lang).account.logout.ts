c7786d0f5bf9c2653b6f361c4867ac84c3b41700
sam <sam.ojling@northvolt.com>
2023-06-13 09:28:05 +0200

    fixing broken logout

--- app/routes/($lang).account.logout.ts
+++ app/routes/($lang).account.logout.ts
@@ -1,10 +1,11 @@
 import {Params} from '@remix-run/react';
 import {
-  redirect,
+  // redirect,
   type ActionFunction,
   type AppLoadContext,
   type LoaderArgs,
   type ActionArgs,
+  json,
 } from '@shopify/remix-oxygen';
 
 export async function doLogout(context: AppLoadContext, params: Params) {
@@ -12,15 +13,19 @@ export async function doLogout(context: AppLoadContext, params: Params) {
   session.unset('customerAccessToken');
 
   // The only file where I have to explicitly type cast i18n to pass typecheck
-  return redirect(`${params.lang}/collections/new`, {
-    headers: {
-      'Set-Cookie': await session.commit(),
+  return json(
+    {status: 'ok'},
+    {
+      headers: {
+        'Set-Cookie': await session.commit(),
+      },
     },
-  });
+  );
 }
 
 export async function loader({params}: LoaderArgs) {
-  return redirect(`${params.lang}/collections/new`);
+  return json({status: 'ok'});
+  // return redirect(`${params.lang}/collections/new`);
 }
 
 export const action: ActionFunction = async ({context, params}: ActionArgs) => {
